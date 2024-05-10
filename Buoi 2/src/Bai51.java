d864491c47b3f01c50cb6bdb89957fd6ca5cddcb
ADMIN <dothanhthi.220299@gmail.com>
2024-05-10 16:27:10 +0700

    commit

---
+++ Buoi 2/src/Bai51.java
@@ -0,0 +1,8 @@
+public class Bai51 {
+    public static void main(String[] args) {
+        int chieudai = 5;
+        int chieurong = 3;
+        int dientich = chieudai*chieurong;
+        System.out.println("Dien tich hinh chu nhat la: "+dientich);
+    }
+}
